445c5c036e60e3554a85289fb4e629897832dd5c
Eric Haydel <erichaydel@gmail.com>
2014-01-27 20:26:05 -0500

    Finished adding pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
-  get "pages/help"
+
+	root 'pages#home'
+	match '/help',    to: 'pages#help',    via: 'get'
+	match '/about',   to: 'pages#about',   via: 'get'
+	match '/contact', to: 'pages#contact', via: 'get'
+	match '/signup',  to: 'users#new', 		 via: 'get'
+	
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
