6f083b69a03fb05079ab6237c6216ce8946f494d
Eric Haydel <erichaydel@gmail.com>
2014-03-17 15:21:03 -0400

    Cleaned up some things

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,7 @@
 class MicropostsController < ApplicationController
 	
-	before_filter :signed_in_user, only: [:create, :destroy]
+	before_action :signed_in_user, only: [:create, :destroy]
+	before_action :correct_user, only: :destroy
 	
 	def create
 		@micropost = current_user.microposts.build(micropost_params)
@@ -14,7 +15,8 @@ class MicropostsController < ApplicationController
 	end
 	
 	def destroy
-	
+		@micropost.destroy
+		redirect_to root_path
 	end
 	
 	private
@@ -22,5 +24,10 @@ class MicropostsController < ApplicationController
     def micropost_params
       params.require(:micropost).permit(:content)
     end
+		
+		def correct_user
+			@micropost = current_user.microposts.find_by_id(params[:id])
+			redirect_to root_path if @micropost.nil?
+		end
 
 end
